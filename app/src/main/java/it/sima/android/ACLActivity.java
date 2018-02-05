456bba7c23b20b9e87c85e5a2609fb823adbe9ef
Gaetano Capizzi <capizzigae@gmail.com>
2018-02-05 15:04:14 +0100

    Aggiunta attività ACL

---
+++ app/src/main/java/it/sima/android/ACLActivity.java
@@ -0,0 +1,19 @@
+package it.sima.android;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 05/02/2018.
+ */
+
+public class ACLActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_acl);
+    }
+}
+
