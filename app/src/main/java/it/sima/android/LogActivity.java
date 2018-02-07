fad7fc38ebc58a0392042c7d9f47a2f41676300c
Samuel Territo <samuelterrito87@gmail.com>
2018-02-07 12:00:45 +0100

    Aggiunta attività Log

---
+++ app/src/main/java/it/sima/android/LogActivity.java
@@ -0,0 +1,18 @@
+package it.sima.android;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Samuel on 07/02/2018.
+ */
+
+public class LogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log);
+    }
+}
