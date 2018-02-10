7249cbf1542f0fda4a04db830bf7b9f447fcd79e
Elia Zammuto <theelixzammuto@gmail.com>
2018-02-10 18:20:11 +0100

    Fixed Camera Activity

--- app/src/main/java/it/sima/android/CameraActivity.java
+++ app/src/main/java/it/sima/android/CameraActivity.java
@@ -4,10 +4,19 @@ import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.IOException;
 
@@ -47,9 +56,33 @@ public class CameraActivity extends AppCompatActivity implements TaskCallback<St
         final CameraActivity activity = this;
         camera.takePicture(null,null, new Camera.PictureCallback() {
             @Override
-            public void onPictureTaken(byte[] bytes, Camera camera) {
-                PostBytesTask task = new PostBytesTask(activity);
-                task.execute(bytes);
+            public void onPictureTaken(final byte[] bytes, final Camera camera) {
+                RequestQueue queue = Volley.newRequestQueue(activity);
+                StringRequest request = new StringRequest(StringRequest.Method.POST, "http://10.0.0.6:8080/face/detect", new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(activity,response,Toast.LENGTH_SHORT).show();
+                        camera.startPreview();
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(activity,"ERRORE! " + error.getMessage(),Toast.LENGTH_SHORT).show();
+                        Log.w("sima",error.networkResponse.toString());
+                    }
+                }){
+                    @Override
+                    public byte[] getBody() throws AuthFailureError {
+                        return bytes;
+                    }
+
+                    @Override
+                    public String getBodyContentType() {
+                        return "image/jpeg";
+                    }
+                };
+                queue.add(request);
+                queue.start();
             }
         });
 
