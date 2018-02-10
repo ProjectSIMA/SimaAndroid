ace49ccdcab7fb9cc3c64b169e1ffcac0fcb6b73
Gaetano Capizzi <capizzigae@gmail.com>
2018-02-10 14:26:39 +0100

    Aggiunta attività ACL, inizio scrittura java, aggiunta string

--- app/src/main/java/it/sima/android/ACLActivity.java
+++ app/src/main/java/it/sima/android/ACLActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 
 /**
  * Created by user on 05/02/2018.
@@ -18,16 +19,18 @@ public class ACLActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acl);
 
-        Button addbutton = findViewById(R.id.button1); //dato nome a pulsante
+        /*Button addbutton = findViewById(R.id.button1); //dato nome a pulsante
         addbutton.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View arg0) {
+                LinearLayout layout = (LinearLayout) findViewById(R.id.layout1);
+                String switchname = .getText().toString();
                 EditText edit_fieldname = findViewById(R.id.edit_fieldname);
                 Bundle bundle = new Bundle();
                 bundle.putString("fieldname", edit_fieldname.getText().toString());
-                // aggiunge lo switch(Da vedere come fare ancora)
+                // aggiungere lo switch(Da vedere come fare ancora)
             }
-        });
+        });*/
     }
 }
 
